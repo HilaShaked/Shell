bbb2c2cf033de117c7d0e1274cbdda9b58f62200
HilaShaked <121008561+HilaShaked@users.noreply.github.com>
2023-10-01 16:42:27 +0300

    Delete external/grep.py

--- external/grep.py
+++
@@ -1,8 +0,0 @@
-import re
-import sys
-
-args = sys.argv
-# Mimic 'grep' using 're' module
-with open('filename', 'r') as f:
-    lines = f.readlines()
-matched_lines = [line for line in lines if re.search('pattern',line)]
